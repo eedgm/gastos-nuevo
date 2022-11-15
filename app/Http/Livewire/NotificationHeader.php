cc6759ac30a2fcd4449f02d41543b22be749bcf0
eedgmnurtech <edegracia@nurtech.org>
2022-11-15 08:36:48 -0500

    Minimal bug

--- app/Http/Livewire/NotificationHeader.php
+++ app/Http/Livewire/NotificationHeader.php
@@ -5,21 +5,26 @@ namespace App\Http\Livewire;
 use Carbon\Carbon;
 use App\Models\Expense;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationHeader extends Component
 {
+    public $user;
+
     public function mount()
     {
-
+        $this->user = Auth::user();
     }
 
     public function render()
     {
+        $accounts = $this->user->accounts->modelKeys();
         $today = Carbon::now();
         $day = $today->toDateString();
         $two_days = $today->add(2, 'day');
 
-        $events = Expense::where('date', '>=', $day.' 00:00:00')
+        $events = Expense::whereIn('account_id', $accounts)
+            ->where('date', '>=', $day.' 00:00:00')
             ->where('date', '<=', $two_days->format('Y-m-d').' 23:59:59')
             ->limit(5)
             ->get();
